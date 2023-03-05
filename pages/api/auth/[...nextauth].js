064f8da63738bc2b68f57f68bd7b08886497c4e2
VladimirFomin90 <vladimirfomin90@yandex.ru>
2023-03-06 00:08:11 +0600

    fix google oath

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,14 +2,19 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '../../../../prisma/client';
+import prisma from '../../../prisma/client';
 
-export default NextAuth({
-	adapter: PrismaAdapter(prisma),
+const adapter = PrismaAdapter(prisma);
+
+export const authOptions = {
+	adapter: adapter,
+	secret: process.env.AUTH_SECRET,
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
-});
+};
+
+export default NextAuth(authOptions);
