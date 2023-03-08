d8aeda564d9b6e92df91adc32e675137c7562355
VladimirFomin90 <vladimirfomin90@yandex.ru>
2023-03-08 22:41:57 +0600

    add AddPost

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
-'use client'
-import AddPost from './components/Addpost';
+'use client';
 
-export default function Home() {
-	return <main>
-		<h1>Hello Next.js13</h1>
-		<AddPost />
-	</main>;
+import Post from './components/Post';
+import AddPost from './components/Addpost';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+//Fetch All posts
+const allPosts = async () => {
+	const response = await axios.get("/api/posts/getPosts")
+	return response.data
+  }
+  
+  export default function Home() {
+	const { data, error, isLoading } = useQuery<PostsType[]>({
+	  queryFn: allPosts,
+	  queryKey: ["posts"],
+	})
+	if (error) return error
+	if (isLoading) return "Loading....."
+
+	return (
+		<div>
+		  <AddPost />
+		  {!data.map((post) => (
+			<Post
+			  key={post.id}
+			  id={post.id}
+			  name={post.user.name}
+			  avatar={post.user.image}
+			  postTitle={post.title}
+			//   comments={post.comments}
+			/>
+		  ))}
+		</div>
+	  )
 }
--- pages/api/posts/addPost.tsx
+++ pages/api/posts/addPost.tsx
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../auth/[...nextauth]';
+import prisma from '../../../prisma/client';
 
 export default async function handler(
 	req: NextApiRequest,
@@ -10,5 +11,30 @@ export default async function handler(
 		const session = await getServerSession(req, res, authOptions);
 		if (!session)
 			return res.status(401).json({ message: 'Sign in to make a post' });
+
+		const title: string = req.body.title;
+
+        // User
+		const prismaUser = await prisma.user.findUnique({
+			where: { email: session?.user?.email },
+		});
+
+        // Check title
+		if (title.length > 240)
+			return res.status(403).json({ message: 'Too long text!' });
+		if (!title.length)
+			return res.status(403).json({ message: 'Write something!' });
+
+		try {
+			const result = await prisma.post.create({
+				data: {
+					title,
+                    userId: prismaUser.id,
+				},
+			});
+            res.status(200).json(result)
+		} catch (error) {
+            res.status(403).json({error: 'Error has occured while making a post'})
+        }
 	}
 }
